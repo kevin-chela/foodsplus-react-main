ccee3392c77b798048a6f3263d76f138c6afa28f
jeffjr2060 <kevinchela714@gmail.com>
2023-05-31 09:22:48 +0300

    img

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -4,7 +4,7 @@ const menu = [
     title: 'butter pancakes',
     category: 'breakfast',
     price: 15.99,
-    img: '../assets/images/item-1.jpg',
+    img: 'assets/images/item-1.jpg',
     desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. `,
     amount: 1,
   },
